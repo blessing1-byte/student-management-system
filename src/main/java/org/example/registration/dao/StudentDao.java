96d3df052aafe73694b22764799100fa7a875456
Udom Blessing <blessingudom402@gmail.com>
2026-09-14 19:33:24 +0100

    made name changes

--- src/main/java/org/example/registration/dao/StudentDao.java
+++ src/main/java/org/example/registration/dao/StudentDao.java
@@ -1,4 +1,4 @@
-package org.example.registration.dao;
+  package org.example.registration.dao;
 
 import org.example.registration.model.Department;
 import org.example.registration.model.StudentModel;
@@ -20,38 +20,38 @@ public class StudentDao {
 
     //create action
 
-public Optional<StudentModel> saveStudent(StudentModel studentModel){
-    //string sql student
-    String sql ="INSERT INTO student(name, email, department)" + "VALUES(?,?,?)";
-    try(
-            Connection conn = DatabaseConnection.getConnection();
-            PreparedStatement preparedStmt = conn.prepareStatement(sql)
+    public Optional<StudentModel> saveStudent(StudentModel studentModel){
+        //string sql student
+        String sql ="INSERT INTO Student(Student_name, Student_email, Student_department)" + "VALUES(?,?,?)";
+        try(
+                Connection conn = DatabaseConnection.getConnection();
+                PreparedStatement preparedStmt = conn.prepareStatement(sql)
 
-            ){
-        preparedStmt.setString(1,studentModel.getName());
-        preparedStmt.setString(2,studentModel.getEmail());
-        preparedStmt.setString(3,studentModel.getDepartment().name());
+        ){
+            preparedStmt.setString(1,studentModel.getName());
+            preparedStmt.setString(2,studentModel.getEmail());
+            preparedStmt.setString(3,studentModel.getDepartment().name());
 
-        //equivalent to result.rows[0]
-        int rowInserted = preparedStmt.executeUpdate();
-        if(rowInserted > 0){
-            System.out.println("student created successfully");
-        }else{
-            System.out.println("failed to create student");
-           return Optional.empty();
+            //equivalent to result.rows[0]
+            int rowInserted = preparedStmt.executeUpdate();
+            if(rowInserted > 0){
+                System.out.println("student created successfully");
+            }else{
+                System.out.println("failed to create student");
+                return Optional.empty();
+            }
+
+        }catch(SQLException e){
+            System.out.println("Database error: "+ e.getMessage());
+            return Optional.empty();
         }
+        return Optional.ofNullable(studentModel);
+    };
 
-    }catch(SQLException e){
-        System.out.println("Database error: "+ e.getMessage());
-return Optional.empty();
-    }
-    return Optional.ofNullable(studentModel);
-};
-
-//read action
+    //read action
     public List<StudentModel> findAll () {
         List<StudentModel> students = new ArrayList<>();
-        String sql = "SELECT * FROM students";
+        String sql = "SELECT * FROM Student";
         try (
                 Connection conn = DatabaseConnection.getConnection();
                 PreparedStatement stmt = conn.prepareStatement(sql);
@@ -61,10 +61,10 @@ return Optional.empty();
             //this simple say after executing the query in result set,move to the next line
             while (resultSet.next()) {
                 StudentModel student = new StudentModel(); //create model
-                student.setId(resultSet.getInt("id"));
-                student.setName(resultSet.getString("name"));
-                student.setEmail(resultSet.getString("email"));
-                student.setDepartment(Department.valueOf(resultSet.getString("department")));
+                student.setId(resultSet.getInt("Student_id"));
+                student.setName(resultSet.getString("Student_name"));
+                student.setEmail(resultSet.getString("Student_email"));
+                student.setDepartment(Department.valueOf(resultSet.getString("Student_department")));
                 students.add(student);
             }
             return students;
@@ -76,20 +76,20 @@ return Optional.empty();
 
     //find by email
     public Optional<StudentModel> findByEmail(String email){
-        String sql = "SELECT id, name, email, department from user WHERE email = ?";
+        String sql = "SELECT Student_id, Student_name, Student_email, Student_department from Student WHERE Student_email = ?";
         try(
                 Connection conn = DatabaseConnection.getConnection();
                 PreparedStatement stmt = conn.prepareStatement(sql);
-                )
+        )
         {
-            stmt.setString(3, email);
+            stmt.setString(1, email);
             try(ResultSet rs = stmt.executeQuery()) {
                 while (rs.next()) {
                     StudentModel student = new StudentModel();
-                    student.setId(rs.getInt("id"));
-                    student.setName(rs.getString("name"));
-                    student.setEmail(rs.getString("email"));
-                    student.setDepartment(Department.valueOf(rs.getString("department")));
+                    student.setId(rs.getInt("Student_id"));
+                    student.setName(rs.getString("Student_name"));
+                    student.setEmail(rs.getString("Student_email"));
+                    student.setDepartment(Department.valueOf(rs.getString("Student_department")));
                     return Optional.of(student);
                 }
             }
@@ -104,20 +104,20 @@ return Optional.empty();
     //update action
     public boolean updateStudent(StudentModel studentModel){
         //string sql student
-        String sql ="UPDATE student SET name=?, email=?, department=? WHERE id=?";
+        String sql ="UPDATE Student SET Student_name=?, Student_email=?, Student_department=? WHERE Student_id=?";
         try(
                 Connection conn = DatabaseConnection.getConnection();
                 PreparedStatement preparedStmt = conn.prepareStatement(sql)
 
         ){
-            preparedStmt.setInt(1,studentModel.getId());
-            preparedStmt.setString(2,studentModel.getName());
-            preparedStmt.setString(3,studentModel.getEmail());
-            preparedStmt.setString(4,studentModel.getDepartment().name());
+            preparedStmt.setString(1,studentModel.getName());
+            preparedStmt.setString(2,studentModel.getEmail());
+            preparedStmt.setString(3,studentModel.getDepartment().name());
+            preparedStmt.setInt(4,studentModel.getId());
 
 
             int rowInserted = preparedStmt.executeUpdate();
-                return rowInserted < 0;
+            return rowInserted > 0;
 
         }catch(SQLException e) {
             System.out.println("Database error: " + e.getMessage());
@@ -127,7 +127,7 @@ return Optional.empty();
 
     //delete action
     public boolean deleteStudent(Integer id) {
-        String sql = "DELETE FROM students WHERE id = ?";
+        String sql = "DELETE FROM Student WHERE Student_id = ?";
 
         try (
                 Connection conn = DatabaseConnection.getConnection();
@@ -144,5 +144,3 @@ return Optional.empty();
     };
 
 }
-
-
